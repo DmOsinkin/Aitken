102298042f0d8adaab10a1f42c137e615b7858fd
osinkindm <dm.osinkin@gmail.com>
2015-10-04 22:32:50 +0300

    V2.0

--- Interpol_Lagrange/main.cpp
+++ Interpol_Lagrange/main.cpp
@@ -3,147 +3,65 @@
 
 using namespace std;
 
-double PolLag(double X_values[], double Y_values[], double x, int pow, int pos) {
-	double Pol;
-	if (pow == 1) {
+class Aitken {
+private:
+	int n;
+	double *X_values;
+	double *Y_values;
+	double step(int n, int m, double x) {
+		if (m == n) return Y_values[n];
+		return 
+			((x - X_values[n]) *
+			step(n + 1, m, x) - 
+			(x - X_values[m]) *
+			step(n, m - 1, x)) /
+			(X_values[m] - X_values[n]);
+	};
+public:
+	Aitken(int n, double *X_values, double *Y_values) {
+		this->n = n;
+		this->X_values = new double[n];
+		this->Y_values = new double[n];
+		for (int i = 0; i<n; i++) 
 		{
-			Pol = (Y_values[pos-1] * (X_values[pos] - x) - Y_values[pos] * (X_values[pos-1] - x)) / (X_values[pos] - X_values[pos-1]);
+			this->X_values[i] = X_values[i];
+			this->Y_values[i] = Y_values[i];
 		}
 	}
-	else {
-		Pol = (PolLag(X_values, Y_values, x, (pow - 1), (pos - 1)) *
-			  (X_values[pos] - x) - 
-			   PolLag(X_values, Y_values, x, (pow - 1), pos) * 
-			  (X_values[pos - pow] - x) /
-			  (X_values[pos] - X_values[pos - pow]));
+	double operator()(double x) {
+		return step(0, n - 1, x);
 	}
-	return Pol;
-}
+};
 
-int main(/*int argc, char* argv[]*/)
-{
-	const int N=11;
-	double x=0.58;
-	//cout << "vvedite znachenie tochki = ";
-	//cin >> x;
+int main() {
+	int n; //кол-во интерполируемых точек в текстовых файлах
+	setlocale(0, "Russian"); 
+	cout << "количество точек интерполяции . . .  ";
+	cin >> n; 
+	cout << "\n\n======================================" << endl;
+	double *X_values = new double[n];
+	double *Y_values = new double[n];
+	//создаем потоки вывода значений Х и Y из файлов
 	ifstream Xin;
 	Xin.open("dataX.txt");
 	ifstream Yin;
 	Yin.open("dataY.txt");
-	double X[11];
-	double Y[11];
-	cout << "X[i]\tY[i]" << endl;
-	for (int i = 0; i < N; i++)
+	//заполняем массив Х и массив Y, выводим на экран
+	cout << "X[n]\tY[n]" << endl;
+	for (int i = 0; i < n; i++)
 	{
-		Xin >> X[i];
-		Yin >> Y[i];
-		//cout << X[i] << "\t" << Y[i] << endl;
+		Xin >> X_values[i];
+		Yin >> Y_values[i];
+		cout << X_values[i] << "\t" << Y_values[i] << endl;
 	};
-	for (int i = 1; i <= 10; i++) {
-		double Example = PolLag(X, Y, x, 2, i);
-		cout<<i<<") " << Example << endl;
-	}
+	cout << "======================================\n\n" << endl;
+	//задаем точку
+	double x;
+	cout << "точкa x . . .  ";
+	cin >> x;
+	//считаем значение по схеме Эйткена
+	Aitken example(n, X_values, Y_values);
+	cout << "Ответ . . . " << example(x) << endl;
 	system("pause");
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//#include <iostream>
-//#include <cmath>
-//#include <fstream>
-//#include <cstdio>
-//
-//using namespace std;
-//
-//
-//double InterpolateLagrangePolynomial(double x, double *x_values, double *y_values, int s)
-//{
-//	double lagrange_pol = 0;
-//	double basics_pol;
-//
-//	for (int i = 0; i < s; i++)
-//	{
-//		basics_pol = 1;
-//		for (int j = 0; j < s; j++)
-//		{
-//			if (j == i) continue;
-//			basics_pol *= (x - x_values[j]) / (x_values[i] - x_values[j]);
-//		}
-//		lagrange_pol += basics_pol*y_values[i];
-//	}
-//	return lagrange_pol;
-//}
-//
-//int main(int argc, char *argv[2]){
-//	double x = 0.58;
-//	double *X;
-//	double *Y;
-//
-//	ifstream Xin(argv[1]);
-//	ifstream Yin(argv[2]);
-//	int k;
-//	cin >> k;
-//	cout << "X[i]\tY[i]" << endl;
-//	for (int i = 0; i < k; i++)
-//	{
-//		Xin >> X[i];
-//		Yin >> Y[i];
-//		cout << X[i] << "\t" << Y[i] << endl << endl;
-//	}
-//
-//	double L=InterpolateLagrangePolynomial(x, X, Y, k);
-//	cout << "otvet = " << L << endl;
-//	return 0;
-//}
-//
-//
-//
-//double testF(double x)
-//{
-//	return  x*x*x + 3 * x*x + 3 * x + 1; // for example
-//}
-//
-//int main1(void)
-//{
-//	const int size = 10;
-//	double x_values[size];
-//	double y_values[size];
-//
-//
-//	for (int i = 0; i < size; i++)
-//	{
-//		x_values[i] = i;
-//		y_values[i] = testF(i);
-//	}
-//
-//	printf("%lf\n", InterpolateLagrangePolynomial(13.6, x_values, y_values, size));
-//
-//	return 0;
-//}
